95642b5666ea39d4b0fc71b410a52f089d573404
Benjamin NIDDAM <benjamin@weaverize.com>
2025-11-28 13:38:04 +0100

    chore: gitignore dist, CI builds it
    - Add /dist to .gitignore
    - Use git add -f in CI to force-add ignored dist/
    - CI is now the single source of truth for builds

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './images/images.module';
-export * from './images/images.service';
-export type { ImageBufferResponse, ImageStreamResponse } from './images/images.service';
-export * from './images/images.controller';
-export * from './images/dto/delete-image.dto';
-export * from './images/dto/get-image.dto';
-export * from './images/interfaces/images-module-options.interface';
-export * from './images/images.constants';
-//# sourceMappingURL=index.d.ts.map
